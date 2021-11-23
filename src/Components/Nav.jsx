1f83ce2029299fcdeb398c0930db4fb15fac3dff
Ambera543 <g.eismontiene@gmail.com>
2021-11-23 14:13:57 +0200

    msg

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -14,17 +14,13 @@ function Nav({ reset, sort }) {
   };
   return (
     <div className="scooter_nav">
-      <span className="px4">Sort BY </span>
+      <div className>Sort By </div>
       <select onChange={selectSort} value={sortValue}>
         <option value=""> Select scooter</option>
-        <option value='kilometres_asc'>
-          Total ride kilometres - 1-9
-        </option>
-        <option value='kilometres_desc'>
-          Total ride kilometres- 9-1
-        </option>
-        <option value='time_asc'> Last use time - 1-9</option>
-        <option value='time_desc'>Last use time - 9-1</option>
+        <option value="kilometres_asc">Total ride kilometres - 1-9</option>
+        <option value="kilometres_desc">Total ride kilometres- 9-1</option>
+        <option value="time_asc"> Last use time - 1-9</option>
+        <option value="time_desc">Last use time - 9-1</option>
       </select>
       <div>
         <button className="btn btn-primary" onClick={resetHandler}>
